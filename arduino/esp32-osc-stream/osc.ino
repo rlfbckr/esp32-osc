970cafd4310fd1e8154eccb4e22d2b7fdfb219c3
Ralf Baecker <rlfbckr@gmail.com>
2024-10-15 15:38:59 +0200

    us + cn

---
+++ arduino/esp32-osc-stream/osc.ino
@@ -0,0 +1,13 @@
+void sendDataOSC() {
+  AsyncUDPMessage udpMsg;
+  OSCMessage oscMsg("/data");
+  
+  oscMsg.add(int(millis())); // sends data
+  oscMsg.add(0.001); // sends data
+  oscMsg.add(100);
+  
+  oscMsg.send(udpMsg);
+  oscMsg.empty();
+  udpOut.broadcastTo(udpMsg, networkOutPort); // sends it too all devices in the network (broadcast)
+
+}
