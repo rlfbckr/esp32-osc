f1d1d225eb45aa0bf376fb6f050a76d620baf65b
Ralf Baecker <rlfbckr@gmail.com>
2024-10-16 19:16:44 +0200

    us + cn

--- arduino/esp32-osc-stream/osc.ino
+++ arduino/esp32-osc-stream/osc.ino
@@ -1,11 +1,13 @@
 void sendDataOSC() {
   AsyncUDPMessage udpMsg;
   OSCMessage oscMsg("/data");
-  
+ 
+  // read sensor data here
+
   // the magic happens here:
-  oscMsg.add(int(millis())); // sends data
-  oscMsg.add(0.001); // sends data
-  oscMsg.add(100);
+  oscMsg.add(int(millis())); // sends data (integer)
+  oscMsg.add(0.001); // sends data (float)
+  oscMsg.add(analogRead(23)); // sends readout fron analog portinteger
   
   oscMsg.send(udpMsg);
   oscMsg.empty();
